ecc144207fcad12b139ad63c548816e2035095ab
majdiShomali <majdi.shomali.1997@gmail.com>
2023-08-29 17:49:45 +0300

    final commit

--- src/pages/dashboard/Statistics.js
+++ src/pages/dashboard/Statistics.js
@@ -156,7 +156,7 @@ const Statistics = () => {
 
 
   return (
-    <div className="mt-3 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-3 3xl:grid-cols-6 p-10">
+    <div className="mt-3 flex flex-wrap gap-6 p-10">
      
      
       <div className="!z-5 relative flex flex-col rounded-[20px] bg-[#E8AA42] bg-clip-border shadow-3xl shadow-shadow-500 dark:!bg-navy-800 dark:text-white dark:shadow-none !flex-row flex-grow items-center rounded-[20px]">
@@ -225,7 +225,7 @@ const Statistics = () => {
           </h4>
         </div>
       </div>
-      <div className="!z-5 relative flex flex-col rounded-[20px] bg-[#E8AA42] bg-clip-border shadow-3xl shadow-shadow-500 dark:!bg-navy-800 dark:text-white dark:shadow-none !flex-row flex-grow items-center rounded-[20px] ">
+      {/* <div className="!z-5 relative flex flex-col rounded-[20px] bg-[#E8AA42] bg-clip-border shadow-3xl shadow-shadow-500 dark:!bg-navy-800 dark:text-white dark:shadow-none !flex-row flex-grow items-center rounded-[20px] ">
         <div className="ml-[18px] flex h-[90px] w-auto flex-row items-center">
           <div className="rounded-full bg-[#f4f7fe] p-3 dark:bg-navy-700">
             <span className="flex items-center text-brand-500 dark:text-white">
@@ -237,11 +237,10 @@ const Statistics = () => {
         <div className="h-50 ml-4 flex w-auto flex-col justify-center">
           <p className="font-dm text-sm font-medium text-white">Total orders</p>
           <h4 className="text-xl font-bold text-[#219D80] dark:text-white">
-            {/* {ordersData.length} */}
           </h4>
         </div>
-      </div>
-      <div className="!z-5 relative flex flex-col rounded-[20px] bg-[#E8AA42] bg-clip-border shadow-3xl shadow-shadow-500 dark:!bg-navy-800 dark:text-white dark:shadow-none !flex-row flex-grow items-center rounded-[20px] ">
+      </div> */}
+      {/* <div className="!z-5 relative flex flex-col rounded-[20px] bg-[#E8AA42] bg-clip-border shadow-3xl shadow-shadow-500 dark:!bg-navy-800 dark:text-white dark:shadow-none !flex-row flex-grow items-center rounded-[20px] ">
         <div className="ml-[18px] flex h-[90px] w-auto flex-row items-center">
           <div className="rounded-full bg-[#f4f7fe] p-3 dark:bg-navy-700">
             <span className="flex items-center text-brand-500 dark:text-white">
@@ -255,10 +254,9 @@ const Statistics = () => {
             Pending Tables
           </p>
           <h4 className="text-xl font-bold text-[#219D80] dark:text-white">
-            {/* {pendingTables.length} */}
           </h4>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
